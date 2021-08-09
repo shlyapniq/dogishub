629d3e585166980ee6cd6ea93f59def3bc8b3328
dogi12 <88685766+dogi12@users.noreply.github.com>
2021-08-09 23:13:43 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,156 @@
+
+
+
+
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+
+
+local Window = Library.CreateLib("Доги Хаб", "Serpent")
+
+
+local discord = Window:NewTab("|Сыллка на дискорд|")
+                   local dc = discord:NewSection("Дискорд discord.gg/cheathouse ")
+
+
+local Hats = Window:NewTab("Хэт скрипты")
+
+
+           local hatss = Hats:NewSection("Скрипты с шляпами")
+
+
+local Troll = Window:NewTab("Тролл скрипты")
+
+
+           local trolls = Troll:NewSection("Изм размера R15 (Параметры аватара в дс)")
+           
+                                          trolls:NewButton("Большой Персонаж", "Делает вашего персонажа Большим (r15)", function()
+                              loadstring(game:HttpGet("https://pastebin.com/raw/ymEzD0rk"))()
+                         
+                         
+                                          end)
+                 
+                  trolls:NewButton("Маленький Персонаж", "Делает вашего персонажа Маленьким (r15)", function()
+                                                loadstring(game:HttpGet("https://pastebin.com/raw/0Sw8XD4L"))()
+                         
+                         
+                                                            end)
+
+                    local hattool = Troll:NewSection("Шляпы в Предметы ( Туллы )")
+                    
+                    
+                                      hattool:NewButton("Аксессуары в Предметы", "Дает вам все акксесуарры в предметы", function()
+                                                loadstring(game:HttpGet("https://pastebin.com/raw/exBkZqq4"))()
+                         
+                         
+                                                            end)
+
+           local anim = Hats:NewSection("Скрипты без шляп (анимации)")
+           
+                   anim:NewButton("Паук (R6)", "Превращяет вас в Паука", function()
+                       
+                       
+                     loadstring(game:HttpGet(('https://pastebin.com/raw/Cu7bKQWN'),true))()  
+                       wait(1)
+                       
+                                                loadstring(game:HttpGetAsync("https://pastebin.com/raw/txsk1LJg"))()
+                         
+                         
+                   end)
+                                                     
+                                                         anim:NewButton("Неко(R6)", "Превращяет вас в Неко кошку", function()
+                       
+                       
+                     loadstring(game:HttpGet(('https://pastebin.com/raw/Cu7bKQWN'),true))()  
+                       wait(1)
+                       
+                                                loadstring(game:HttpGetAsync("https://hastebin.com/raw/desuvawopu"))()
+                         
+                         
+                                                            end)
+                             
+                             
+                             
+                              hatss:NewButton("Мусорное ведро (R6) (Rare)", "Превращяет вас в Мусорное ведро", function()
+                       
+                       
+                     loadstring(game:HttpGet(('https://pastebin.com/raw/Cu7bKQWN'),true))()  
+                       wait(1)
+                       
+                                                loadstring(game:HttpGetAsync("https://hastebin.com/raw/yabojojeju"))()
+                         
+                         
+                              end)
+               
+               
+               
+                                         
+                              hatss:NewButton("Кекс Кукла (R6)(Rare)", "Скрипт на секс куклу", function()
+                       
+                       
+                     
+                       wait(1)
+                       
+                                                loadstring(game:HttpGetAsync("https://hastebin.com/raw/cebukagisu"))()
+                         
+                         
+                              end)
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+local adm = Window:NewTab("Админы")
+
+
+          local adm1 = adm:NewSection("Админ Скрипты")
+          
+                      
+                      
+                      
+                      
+                       adm1:NewButton("Fates Админ ", "Fate Admin", function()
+                       
+                   
+                                   loadstring(game:HttpGet("https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua"))();
+
+                         
+                         
+                       end)
+                          
+                          
+                       adm1:NewButton("YI Админ ", "Yinfinity yield", function()
+                       
+                   
+                                   loadstring(game:HttpGetAsync("https://hastebin.com/raw/yabojojeju"))()
+                         
+                         
+                       end)
+                          
+                          
+                       adm1:NewButton(" Мусорное ведро (R6) ", "Превращяет вас в Мусорное ведро", function()
+                       
+                   
+                                     loadstring(game:HttpGetAsync("https://hastebin.com/raw/yabojojeju"))()
+                         
+                         
+                              end)
+               
